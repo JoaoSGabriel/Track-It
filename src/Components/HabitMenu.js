d4467039cd07c856885b82408bf828521af55cb3
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-08-05 10:55:17 -0300

    link API send habit

--- src/Components/HabitMenu.js
+++ src/Components/HabitMenu.js
@@ -1,23 +1,75 @@
+import { useContext, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+import UserContext from "./contexts/UserContext";
+
+function WeekDay (props) {
+    const {letter, index, arr_Days, setArr_Days} = props;
+    const [is_Select, setIs_Select] = useState(false);
+
+    function selectDay (text){
+        if (text === true) {
+            setIs_Select(!is_Select);
+            for (let i = 0; i < arr_Days.length; i = i + 1) {
+                if (arr_Days[i] === index) {
+                    arr_Days.splice(i, 1);
+                }
+            }
+        }
+        if (text === false) {
+            setIs_Select(!is_Select);
+            setArr_Days([...arr_Days, index]);
+        }
+    }
+
+    return(
+        <>
+        {is_Select === false ? (
+            <p onClick={() => selectDay(is_Select)}>{letter}</p>
+        ) : (
+            <h1 onClick={() => selectDay(is_Select)}>{letter}</h1>
+        )}
+        </>
+    );
+}
+
+const week_Days = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 export default function HabitMenu (props) {
     const {setCreate_Habit} = props;
+    const {user_Token} = useContext(UserContext);
+
+    const [arr_Days, setArr_Days] = useState([]);
+    const [habit_Name, setHabit_Name] = useState('');
+
+    function sendHabit () {
+        const bodyRequest = {
+            name: habit_Name,
+            days: arr_Days
+        }
+        const promisse = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits', bodyRequest, {
+            headers: {
+                Authorization: `Bearer ${user_Token}`
+            }
+        });
+        promisse.then(() => {
+            setArr_Days([]);
+            setHabit_Name('');
+            setCreate_Habit(false);
+        })
+        promisse.catch();
+    }
+
     return(
         <>
             <Menu>
-                <input placeholder="  nome do hábito"></input>
+                <input type="text" placeholder="  nome do hábito" onChange={e => setHabit_Name(e.target.value)} value={habit_Name}></input>
                 <Days>
-                    <p>D</p>
-                    <p>S</p>
-                    <p>T</p>
-                    <p>Q</p>
-                    <p>Q</p>
-                    <p>S</p>
-                    <p>S</p>
+                    {week_Days.map((item, index) => <WeekDay key={index} letter={item} index={index} arr_Days={arr_Days} setArr_Days={setArr_Days}/>)}
                 </Days>
                 <Buttons>
                     <Cancel onClick={() => setCreate_Habit(false)}>Cancelar</Cancel>
-                    <Save>Salvar</Save>
+                    <Save onClick={sendHabit}>Salvar</Save>
                 </Buttons>
             </Menu>
         </>
@@ -64,6 +116,23 @@ const Days = styled.div`
         line-height: 24.97px;
         color: #DBDBDB;
         cursor: pointer;
+    }
+    h1 {
+        width: 30px;
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        border: 1px solid #CFCFCF;
+        margin: 0 4px 0 0;
+        font-family: 'Lexend Deca';
+        font-weight: 400;
+        font-size: 19.98px;
+        line-height: 24.97px;
+        color: #FFFFFF;
+        cursor: pointer;
+        background-color: #CFCFCF;
     }
 `;
 
